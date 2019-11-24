0484d65ac27ed91d33abff951f0ac44abd94ce49
wangyu <979222162@qq.com>
2019-11-24 22:18:06 +0800

    first commit

---
+++ notadd-ngx-neditor.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { NeditorConfig as ɵb } from './lib/neditor.config';
+export { ScriptLoaderService as ɵa } from './lib/script-loader.service';
